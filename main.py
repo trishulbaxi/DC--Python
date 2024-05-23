5a376df6b7012a629ae2b842fd0e8eea91a2b121
trishul.baxi <trishul.baxi@truedigital.com>
2024-05-23 13:11:22 +0700

    added comments

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
-from netmiko import ConnectHandler
-from getpass import getpass
+from netmiko import ConnectHandler  # use netmiko for SSH connections.
+from getpass import getpass  # User needs to lodgin. Avoids storing passwords in plain text.
 
-password = getpass()
+password = getpass()   # create a variable to store the password input.
 
+# Define the device connection parameters in a list.
 sw1 = {
     'device_type': "cisco_nxos",
     'ip': '192.168.90.3',
@@ -12,8 +13,9 @@ sw1 = {
     #'secret': password,
 }
 
+# Connect to the device and apply the configuration.
 net_connect = ConnectHandler(**sw1)
 net_connect.enable()
 config_commands = ['int loop 0', 'ip address 1.1.1.1 255.255.255.255', 'Description Python Loopback']
 output = net_connect.send_config_set(config_commands)
-print (output)
+print (output)  # Print the output to your screen.
